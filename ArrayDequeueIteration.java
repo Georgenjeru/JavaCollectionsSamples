4a2a7b91e314aeb347333ed1ddfe489998a90d4b
Georgenjeru <njerugeorge57@gmail.com>
2022-09-22 00:13:53 +0300

    JavaCollections

---
+++ ArrayDequeueIteration.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+class ArrayDequeueS {
+    public static void main(String[] args) {
+        ArrayDeque<String> animals= new ArrayDeque<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.print("ArrayDeque: ");
+
+        // Using iterator()
+        Iterator<String> iterate = animals.iterator();
+        while(iterate.hasNext()) {
+            System.out.print(iterate.next());
+            System.out.print(", ");
+        }
+
+        System.out.print("\nArrayDeque in reverse order: ");
+        // Using descendingIterator()
+        Iterator<String> desIterate = animals.descendingIterator();
+        while(desIterate.hasNext()) {
+            System.out.print(desIterate.next());
+            System.out.print(", ");
+        }
+    }
+}
